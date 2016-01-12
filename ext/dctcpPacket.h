3cf74265be52fc4ab178045ea2a5bc49f38888e4
Akshay <akshay.narayan@berkeley.edu>
2016-01-12 14:57:24 -0800

    added dctcp flow impl

--- ext/dctcpPacket.h
+++ ext/dctcpPacket.h
@@ -19,4 +19,27 @@ class DctcpPacket : public Packet {
         ) : Packet(sending_time, flow, seq_no, pf_priority, size, src, dst) {
             this->ecn = ecn;
         }
-}
+};
+
+class DctcpAck : public Ack {
+    public:
+        bool ecn;
+        uint32_t delayed_num;
+
+        DctcpAck(
+            Flow *flow, 
+            uint32_t seq_no_acked, 
+            std::vector<uint32_t> sack_list,
+            uint32_t size,
+            Host* src,
+            Host* dst,
+            bool ecn,
+            uint32_t delayed_num
+        ) : Ack(flow, seq_no_acked, sack_list, size, src, dst) {
+            this->ecn = ecn;
+            this->delayed_num = delayed_num;
+        }
+};
+
+
+#endif
